fd75fc62d1f7316d0e1ce7ab31588e5a56aa16d7
Dave Avis <charles.avis@springbranchisd.com>
2021-03-10 23:23:28 +0000

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Lab-23-Shape-War
+# Lab 23 - Shape War
+
+This is not for a grade.  Today we will be getting a small introduction to the FXGL game library for Java by writing a small game together.
---
+++ ShapeWarFactory.java
@@ -0,0 +1,19 @@
+import static com.almasb.fxgl.dsl.FXGL.entityBuilder;
+import static com.almasb.fxgl.dsl.FXGL.getAppHeight;
+import static com.almasb.fxgl.dsl.FXGL.getAppWidth;
+import static com.almasb.fxgl.dsl.FXGL.getInput;
+import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameWorld;
+
+import com.almasb.fxgl.dsl.components.OffscreenCleanComponent;
+import com.almasb.fxgl.dsl.components.ProjectileComponent;
+import com.almasb.fxgl.dsl.components.RandomMoveComponent;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.Spawns;
+import javafx.geometry.Point2D;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
